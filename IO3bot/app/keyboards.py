bf508dbc33ce11b789da51ab511b14acd407082c
itshnikisobuhovki <127012269+itshnikisobuhovki@users.noreply.github.com>
2024-09-29 16:55:43 +0300

    Add files via upload

---
+++ IO3bot/app/keyboards.py
@@ -0,0 +1,42 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
+from aiogram.utils import keyboard
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
+# main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Пошук на мапі')]
+#                                      ], one_time_keyboard=True,
+#                                           resize_keyboard=True,
+#                 input_field_placeholder="введіть адресу у довільній формі")
+
+main = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='XXX', callback_data="find_on_map")]])
+# property_types = ['квартира', 'будинок', 'земельна_ділянка', 'магазин_салон', 'ресторан_кафе',
+#                   'офіс', 'склад', 'пром_база', 'виробництво', 'база_відпочинку', 'готель',
+#                   'МАФ_кіоск', 'точка_на_базарі', 'АЗС', 'автомийка', 'СТО',
+#                   'фермерське_господарство', 'гараж_паркувальне_місце',]
+#
+#
+#
+transaction_types = ['ОРЕНДА', 'ПРОДАЖ']
+#
+# async def reply_property_types():
+#     keyboard = ReplyKeyboardBuilder()
+#     for property_type in property_types:
+#         keyboard.add(KeyboardButton(text=property_type))
+#     return keyboard.adjust(4).as_markup()
+#
+# async def inline_property_types():
+#     keyboard = InlineKeyboardBuilder()
+#     for property_type in property_types:
+#         keyboard.add(InlineKeyboardButton(text=property_type, callback_data=property_type))
+#     return keyboard.adjust(3).as_markup()
+
+# main = InlineKeyboardMarkup(inline_keyboard=[
+#     [InlineKeyboardButton(text='Пошук на мапі', callback_data='find_on_map')]
+# ])
+#
+cur = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Євро', callback_data='EUR')],
+    [InlineKeyboardButton(text='Долари США', callback_data='USD')],
+    [InlineKeyboardButton(text='Гривні', callback_data='UAGRN')],
+])
